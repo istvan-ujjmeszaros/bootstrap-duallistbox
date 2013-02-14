652d1bcfdd205ffe14e073053b464bc3008b5128
Istvan Ujj-Meszaros <styu007@gmail.com>
2013-02-14 04:40:42 +0100

    Initial release.

---
+++ bootstrap-duallistbox/jquery.bootstrap-duallistbox.js
@@ -0,0 +1,399 @@
+/*jshint undef: true, unused:true */
+/*global jQuery: true */
+
+/*!=========================================================================
+ *  Bootstrap Dual Listbox
+ *  v1.0.0
+ *
+ *  Responsive dual multiple select with filtering. Designed to work on
+ *  small touch devices.
+ *
+ *  https://github.com/istvan-meszaros/bootstrap-duallistbox
+ *  http://www.virtuosoft.eu/code/bootstrap-duallistbox/
+ *
+ *  Copyright 2013 István Ujj-Mészáros
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ====================================================================== */
+
+(function($) {
+    "use strict";
+
+    $.fn.bootstrapDualListbox = function(options) {
+
+        return this.each(function() {
+            if (!$(this).is("select"))
+            {
+                return $(this).find("select").each(function() {
+                    $(this).bootstrapDualListbox();
+                });
+            }
+
+            if ($(this).data('duallistbox_generated'))
+                return this;
+
+            var settings = $.extend( {
+                preserveselectiononmove : false,            // 'all' / 'moved' / false
+                moveonselect            : true,             // true/false (forced true on androids, see the comment later)
+                initialfilterfrom       : '',               // string, filter selectables list on init
+                initialfilterto         : '',               // string, filter selected list on init
+                helperselectnamepostfix     : '_helper',        // 'string_of_postfix' / false
+                infotext                : 'Showing all {0}',// text when all options are visible / false for no info text
+                infotextfiltered        : '<span class="label label-warning">Filtered</span> {0} from {1}',// when not all of the options are visible due to the filter
+                infotextempty           : 'Empty list'      // when there are no options present in the list
+            }, options);
+
+            var container = $('<div class="row-fluid bootstrap-duallistbox-container"><div class="span6 box1"><span class="info-container"><span class="info"></span><button class="btn btn-mini clear1 pull-right">X</button></span><input placeholder="Filter" class="filter" type="text"><div class="btn-group buttons"><button class="btn moveall" type="button" title="Move all"><i class="icon-arrow-right"></i><i class="icon-arrow-right"></i></button><button class="btn move" type="button" title="Move selected"><i class="icon-arrow-right"></i></button></div><select multiple="multiple" data-duallistbox_generated="true"></select></div><div class="span6 box2"><span class="info-container"><span class="info"></span><button class="btn btn-mini clear2 pull-right">x</button></span><input placeholder="Filter" class="filter" type="text"><div class="btn-group buttons"><button class="btn remove" type="button" title="Remove selected"><i class="icon-arrow-left"></i></button><button class="btn removeall" type="button" title="Remove all"><i class="icon-arrow-left"></i><i class="icon-arrow-left"></i></button></div><select multiple="multiple" data-duallistbox_generated="true"></select></div></div>').insertBefore($(this)),
+                elements = {
+                    originalselect: $(this),
+                    box1: $('.box1', container),
+                    box2: $('.box2', container),
+                    filterinput1: $('.box1 .filter', container),
+                    filterinput2: $('.box2 .filter', container),
+                    filter1clear: $('.box1 .clear1', container),
+                    filter2clear: $('.box2 .clear2', container),
+                    info1: $('.box1 .info', container),
+                    info2: $('.box2 .info', container),
+                    select1: $('.box1 select', container),
+                    select2: $('.box2 select', container),
+                    movebutton: $('.box1 .move', container),
+                    removebutton: $('.box2 .remove', container),
+                    moveallbutton: $('.box1 .moveall', container),
+                    removeallbutton: $('.box2 .removeall', container),
+                    form: $($('.box1 .filter', container)[0].form)
+                },
+                i = 0,
+                selectedelements = 0,
+                numberofelements = elements.originalselect.find('option').length,
+                // Selections are invisible on android if the containing select is styled with CSS
+                // http://code.google.com/p/android/issues/detail?id=16922
+                isbuggyandroid = /android/i.test(navigator.userAgent.toLowerCase());
+
+            init();
+
+            function init()
+            {
+                // We are forcing to move on select and disabling preserveselection on Android
+                if (isbuggyandroid) {
+                    settings.moveonselect = true;
+                    settings.preserveselectiononmove = false;
+                }
+
+                if (settings.moveonselect) {
+                    container.addClass('moveonselect');
+                }
+
+                var originalselectname = elements.originalselect.attr('name') || '';
+
+                if (!!settings.helperselectnamepostfix)
+                {
+                    elements.select1.attr('name', originalselectname + settings.helperselectnamepostfix + '1');
+                    elements.select2.attr('name', originalselectname + settings.helperselectnamepostfix + '2');
+                }
+
+                var c = elements.originalselect.attr('class').match(/\bspan[1-9][0-2]?/);
+                if (!!c) {
+                    container.addClass(c.toString());
+                }
+
+                elements.select1.height(elements.originalselect.height());
+                elements.select2.height(elements.originalselect.height());
+
+                elements.originalselect.css('display', 'none').find('option').each(function() {
+                    $(this).data('original-index', i++);
+                    $(this).data('_selected', false);
+                });
+
+                elements.filterinput1.val(settings.initialfilterfrom);
+                elements.filterinput2.val(settings.initialfilterto);
+
+                bindevents();
+                refreshselects();
+            }
+
+            function refreshselects()
+            {
+                selectedelements = 0;
+
+                elements.select1.empty();
+                elements.select2.empty();
+
+                elements.originalselect.find('option').each(function() {
+                    if ($(this).prop('selected')) {
+                        selectedelements++;
+                        elements.select2.append($(this).clone(true).prop('selected', $(this).data('_selected')));
+                    }
+                    else {
+                        elements.select1.append($(this).clone(true).prop('selected', $(this).data('_selected')));
+                    }
+                });
+
+                filter1();
+                filter2();
+
+                refreshinfo();
+            }
+
+            function formatstring(s, args)
+            {
+                return s.replace(/\{(\d+)\}/g, function(match, number) {
+                    return typeof args[number] !== 'undefined' ? args[number] : match;
+                });
+            }
+
+            function refreshinfo()
+            {
+                if (settings.infotext === false) {
+                    return;
+                }
+
+                var visible1 = elements.select1.find('option').length,
+                    visible2 = elements.select2.find('option').length,
+                    all1 = numberofelements - selectedelements,
+                    all2 = selectedelements;
+
+                if (all1 === 0) {
+                    elements.info1.html(settings.infotextempty);
+                    elements.box1.removeClass("filtered");
+                }
+                else if (visible1 === all1) {
+                    elements.info1.html(formatstring(settings.infotext, [visible1, all1]));
+                    elements.box1.removeClass("filtered");
+                }
+                else {
+                    elements.info1.html(formatstring(settings.infotextfiltered, [visible1, all1]));
+                    elements.box1.addClass("filtered");
+                }
+
+                if (all2 === 0) {
+                    elements.info2.html(settings.infotextempty);
+                    elements.box2.removeClass("filtered");
+                }
+                else if (visible2 === all2) {
+                    elements.info2.html(formatstring(settings.infotext, [visible2, all2]));
+                    elements.box2.removeClass("filtered");
+                }
+                else {
+                    elements.info2.html(formatstring(settings.infotextfiltered, [visible2, all2]));
+                    elements.box2.addClass("filtered");
+                }
+            }
+
+            function bindevents()
+            {
+                elements.form.submit(function(e) {
+                    if (elements.filterinput1.is(":focus"))
+                    {
+                        e.preventDefault();
+                        elements.filterinput1.focusout();
+                    }
+                    else if (elements.filterinput2.is(":focus"))
+                    {
+                        e.preventDefault();
+                        elements.filterinput2.focusout();
+                    }
+                });
+
+                elements.filter1clear.on('click', function() {
+                    elements.filterinput1.val('');
+                    refreshselects();
+                });
+
+                elements.filter2clear.on('click', function() {
+                    elements.filterinput2.val('');
+                    refreshselects();
+                });
+
+                elements.movebutton.on('click', function() {
+                    move();
+                });
+
+                elements.moveallbutton.on('click', function() {
+                    moveall();
+                });
+
+                elements.removebutton.on('click', function() {
+                    remove();
+                });
+
+                elements.removeallbutton.on('click', function() {
+                    removeall();
+                });
+
+                elements.filterinput1.on('change keyup', function() {
+                    filter1();
+                });
+
+                elements.filterinput2.on('change keyup', function() {
+                    filter2();
+                });
+
+                if (settings.moveonselect) {
+                    settings.preserveselectiononmove = false;
+
+                    elements.select1.on('change', function() {
+                        move();
+                    });
+                    elements.select2.on('change', function() {
+                        remove();
+                    });
+                }
+
+            }
+
+            function saveselections1()
+            {
+                elements.select1.find('option').each(function() {
+                    elements.originalselect.find('option').eq($(this).data('original-index')).data('_selected', $(this).prop('selected'));
+                });
+            }
+
+            function saveselections2()
+            {
+                elements.select2.find('option').each(function() {
+                    elements.originalselect.find('option').eq($(this).data('original-index')).data('_selected', $(this).prop('selected'));
+                });
+            }
+
+            function filter1() {
+                saveselections1();
+
+                elements.select1.empty().scrollTop(0);
+
+                var regex = new RegExp($.trim(elements.filterinput1.val()), "gi");
+
+                elements.originalselect.find('option').not(':selected').each(function() {
+                    if (this.text.match(regex) !== null) {
+                        elements.originalselect.find('option').eq($(this).data('original-index')).data('filtered1', false);
+                        elements.select1.append($(this).clone(true).prop('selected', $(this).data('_selected')));
+                    }
+                    else {
+                        elements.originalselect.find('option').eq($(this).data('original-index')).data('filtered1', true);
+                    }
+                });
+
+                refreshinfo();
+            }
+
+            function filter2() {
+                saveselections2();
+
+                elements.select2.empty().scrollTop(0);
+
+                var regex = new RegExp($.trim(elements.filterinput2.val()), "gi");
+
+                elements.originalselect.find('option:selected').each(function() {
+                    if (this.text.match(regex) !== null) {
+                        elements.originalselect.find('option').eq($(this).data('original-index')).data('filtered2', false);
+                        elements.select2.append($(this).clone(true).prop('selected', $(this).data('_selected')));
+                    }
+                    else {
+                        elements.originalselect.find('option').eq($(this).data('original-index')).data('filtered2', true);
+                    }
+                });
+
+                refreshinfo();
+            }
+
+            function sortoptions(select)
+            {
+                select.find('option').sort(function(a, b) {
+                    return ($(a).data('original-index') > $(b).data('original-index')) ? 1 : -1;
+                }).appendTo(select);
+            }
+
+            function changeselectionstate(original_index, selected)
+            {
+                elements.originalselect.find('option').each(function() {
+                    if ($(this).data('original-index') === original_index) {
+                        $(this).prop('selected', selected);
+                    }
+                });
+            }
+
+            function move()
+            {
+                if (settings.preserveselectiononmove === 'all') {
+                    saveselections1();
+                    saveselections2();
+                }
+                else if (settings.preserveselectiononmove === 'moved') {
+                    saveselections1();
+                }
+
+                elements.select1.find('option:selected').each(function() {
+                    if (!$(this).data('filtered1')) {
+                        changeselectionstate($(this).data('original-index'), true);
+                    }
+                });
+
+                refreshselects();
+
+                sortoptions(elements.select2);
+            }
+
+            function remove()
+            {
+                if (settings.preserveselectiononmove === 'all') {
+                    saveselections1();
+                    saveselections2();
+                }
+                else if (settings.preserveselectiononmove === 'moved') {
+                    saveselections2();
+                }
+
+                elements.select2.find('option:selected').each(function() {
+                    if (!$(this).data('filtered2')) {
+                        changeselectionstate($(this).data('original-index'), false);
+                    }
+                });
+
+                refreshselects();
+
+                sortoptions(elements.select1);
+            }
+
+            function moveall()
+            {
+                if (settings.preserveselectiononmove) {
+                    saveselections();
+                }
+
+                elements.originalselect.find('option').each(function() {
+                    if (!$(this).data('filtered1')) {
+                        $(this).prop('selected', true);
+                    }
+                });
+
+                refreshselects();
+            }
+
+            function removeall()
+            {
+                if (settings.preserveselectiononmove) {
+                    saveselections();
+                }
+
+                elements.originalselect.find('option').each(function() {
+                    if (!$(this).data('filtered2')) {
+                        $(this).prop('selected', false);
+                    }
+                });
+
+                refreshselects();
+            }
+        });
+
+    };
+
+})(jQuery);
