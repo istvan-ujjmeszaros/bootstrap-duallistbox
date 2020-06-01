979efbb79ac9a9caa00a85052dff511f0174df9d
XhmikosR <xhmikosr@gmail.com>
2020-06-01 14:21:58 +0300

    Remove console.log method call

--- src/jquery.bootstrap-duallistbox.js
+++ src/jquery.bootstrap-duallistbox.js
@@ -101,7 +101,6 @@
   }
 
   function formatString(s, args) {
-    console.log(s, args);
     return s.replace(/{(\d+)}/g, function(match, number) {
       return typeof args[number] !== 'undefined' ? args[number] : match;
     });
